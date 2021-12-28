3f356bd14cf52b8abec1a9f2bc2be7d2e382c248
Jackse Nogueira <94062159+JackseNS@users.noreply.github.com>
2021-12-28 10:02:27 -0300

    Update hackers.sh

--- hackers.sh
+++ hackers.sh
@@ -1,1 +1,37 @@
+```
+$ apt update
+
+```
+
+```
+$ apt upgrade
+```
+
+## Link para gerar o zphisher
+
+```
+$ git clone git://github.com/htr-tech/zphisher.git
+```
+```
+$ ls
+```
+```
+$ cd zphisher
+```
+```
+$ ls
+```
+```
+$ bash zphisher.sh
+```
+## Selecione a opção escolhida
+
+- Escolha a redesocial escolhida.Exemplo:
+```
+1
+```
+```
+2
+```
+## pegue um dos links url
 
